69c261f71cfb9041b4afa6169f9aab5e910456f6
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-10 17:55:12 +0100

    fix: :white_check_mark: Handle negative range list creation

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,6 @@ from typing import List
 class ListFactory(ABC):
     @staticmethod
     def create_list(n: int) -> List[int]:
-        if n == 0:
+        if n <= 0:
             raise ValueError("n must be greater than 0")
         return list(range(1, n + 1))
