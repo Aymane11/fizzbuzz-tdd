48ded66ae3df0a5024b4afac4b0c3fc770e9e695
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-10 17:43:34 +0100

    test: :test_tube: Added Failing test for range 3 list creation

--- tests/test_listfactory.py
+++ tests/test_listfactory.py
@@ -1,6 +1,10 @@
 from src.utils import ListFactory
 
 
-def testOneItemList():
+def testRange1List():
 	factory = ListFactory.create_list(1)
 	assert factory == [1]
+
+def testRange3List():
+	factory = ListFactory.create_list(3)
+	assert factory == [1, 2, 3]
