f0f530d3e8718a70a8cb3fbdf28dee04908c1769
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-10 18:11:37 +0100

    test: :test_tube: Add failing test convert 5 to Buzz

--- tests/test_fizzbuzz_converter.py
+++ tests/test_fizzbuzz_converter.py
@@ -6,3 +6,6 @@ def testConvert1ShouldBe1():
 
 def testConvert3ShouldBeFizz():
 	assert FizzBuzzConverter.convert(3) == "Fizz"
+
+def testConvert5ShouldBeBuzz():
+	assert FizzBuzzConverter.convert(5) == "Buzz"
