17625dcf3fed4969b9f67f28134dd3c59290d13e
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-10 18:09:15 +0100

    test: :test_tube: Add failing test for convert 3

--- tests/test_fizzbuzz_converter.py
+++ tests/test_fizzbuzz_converter.py
@@ -1,5 +1,8 @@
 from src.fizzbuzz import FizzBuzzConverter
 
 
-def testConvert1():
+def testConvert1ShouldBe1():
     assert FizzBuzzConverter.convert(1) == "1"
+
+def testConvert3ShouldBeFizz():
+	assert FizzBuzzConverter.convert(3) == "Fizz"
