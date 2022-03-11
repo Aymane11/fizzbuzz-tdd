1481fc93f8e096498c48ede77c42de87eca6ab16
Aymane Boumaaza <aymaneboumaaza@gmail.com>
2022-03-11 15:29:59 +0100

    test: :white_check_mark: Added passing test for fizzbuzz engine range 15

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -14,3 +14,23 @@ def test_fizzbuzz_range_10():
         "Fizz",
         "Buzz",
     ]
+
+
+def test_fizzbuzz_range_15():
+    assert FizzBuzzEngine.fizzbuzz(15) == [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz",
+    ]
